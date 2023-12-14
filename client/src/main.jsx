ed8e3b88c9621487c96db36e264a9747e7ed2ef0
l3alisidahmed <sidoabdelali06@gmail.com>
2023-12-14 19:57:09 +0100

    change the style of main page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// import CreateRoom from './Pages/CreateRoom.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <CreateRoom /> */}
   </React.StrictMode>,
 )
